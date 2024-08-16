819332c27b52b9f81da8a604143ab122f618bd6e
Sectarmus <nizamoglualper@hotmail.com>
2024-08-16 17:33:40 +0300

    bootstrap changes

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react'
-import Book from './Book'
+import React from 'react';
+import './BookList.css';
+import Book from './Book';
 
-export default class BookList extends Component {
-  render() {
+class BookList extends React.Component {
+    render() {
+        // console.log(this.props.books)
+        const bookList = this.props.books.map((book, i) => {
+            return <Book book={book}
+                key={i}
+            />
+        })
+        return (
+            <section className="page-section bg-light" id="portfolio">
+                <div className="container">
+                    <div className="text-center">
+                        <h2 className="section-heading text-uppercase">BookFolio</h2>
+                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    </div>
+                    <div className="row">
 
-    const bookList = this.props.books.map((book,i) => {
-        return <Book book = {book} key = {i} />
-    })
+                        {bookList}
 
-    return (
-      <div>
-        {bookList}
-      </div>
-    )
-  }
+                    </div>
+                </div>
+            </section>
+        )
+    }
+
 }
+
+export default BookList;
