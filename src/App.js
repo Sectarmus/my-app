206d50dae66769af98b87a098da79e18fd2c790b
Sectarmus <nizamoglualper@hotmail.com>
2024-08-19 02:15:25 +0300

    multiple context

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 import BookList from "./components/BookList";
 import BookContextProvider from "./contexts/BookContext";
+import ThemeContextContextProvider from "./contexts/ThemeContext";
 
 export default class App extends Component {
-  
   render() {
     return (
-      <div>
+      <ThemeContextContextProvider>
         <BookContextProvider>
           <BookList />
         </BookContextProvider>
-        
-      </div>
+      </ThemeContextContextProvider>
     );
   }
 }
-
